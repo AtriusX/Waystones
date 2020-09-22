c9455c094e3ddf53b61b4990bcdce945eacc73e0
Atrius <atriusedm@gmail.com>
2020-09-22 01:30:14 -0400

    WarpNameEvent bugfix
    Fixed an issue allowing warps to be named with unnamed name tags

--- src/main/kotlin/xyz/atrius/waystones/event/WaystoneNameEvent.kt
+++ src/main/kotlin/xyz/atrius/waystones/event/WaystoneNameEvent.kt
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK
 import org.bukkit.event.player.PlayerInteractEvent
 import xyz.atrius.waystones.service.WarpNameService
+import xyz.atrius.waystones.utility.cancel
 import xyz.atrius.waystones.utility.sendActionMessage
 
 class WaystoneNameEvent(private val names: WarpNameService) : Listener {
@@ -26,14 +27,13 @@ class WaystoneNameEvent(private val names: WarpNameService) : Listener {
         if (block?.type != Material.LODESTONE)
             return
         val meta = item.itemMeta
-        if (meta?.displayName == null)
+        if (meta?.hasDisplayName() != true)
             return
-
         names.add(block.location, meta.displayName)
         player.sendActionMessage("Waystone name set to ${meta.displayName}", ChatColor.AQUA)
         player.playSound(player.location, Sound.BLOCK_RESPAWN_ANCHOR_SET_SPAWN, 1f, 2f)
         if (player.gameMode != GameMode.CREATIVE)
             item.amount--
-        event.isCancelled = true
+        event.cancel()
     }
 }
