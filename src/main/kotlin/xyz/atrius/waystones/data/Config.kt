598138235ab2428ed7c6e355ddf8f5ffaf83ce62
Atrius <atriusedm@gmail.com>
2020-09-22 01:30:38 -0400

    Add debuff options and reorganize config

--- src/main/kotlin/xyz/atrius/waystones/data/Config.kt
+++ src/main/kotlin/xyz/atrius/waystones/data/Config.kt
@@ -7,14 +7,13 @@ import xyz.atrius.waystones.utility.valueOfOrDefault
 import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
-@Suppress("unused")
 class Config(
     private val plugin: KotlinPlugin
 ) {
     private val config: FileConfiguration
         get() = plugin.config
 
-    var waitTime by property("wait-time", 60)
+    var waitTime: Int by property("wait-time", 60)
 
     // The max number of blocks allowed in warp structures
     var maxWarpSize: Int by property("max-warp-size", 25)
@@ -25,14 +24,14 @@ class Config(
     // Play animations during teleport sequence
     var warpAnimations: Boolean by property("warp-animations", true)
 
+    // Determines if teleporters limit their max distance or not, will check if user is in range before teleporting if true
+    var limitDistance: Boolean by property("limit-distance", true)
+
     // The minimum base range a waystone can have
     var baseDistance: Int by property("base-distance", 100)
 
     // The maximum amount of blocks a portal block can boost the range of a portal (this is for netherite)
     var maxBoost: Int by property("max-boost", 50)
-
-    // Determines if teleporters limit their max distance or not, will check if user is in range before teleporting if true
-    var limitDistance: Boolean by property("limit-distance", true)
 
     // Determines whether or not warping with a compass deletes the item after use
     var singleUse: Boolean by property("single-use", false)
@@ -42,6 +41,12 @@ class Config(
 
     // How many blocks it takes to travel one block in the destination world
     var worldRatio: Int by property("world-ratio", 8)
+
+    // Whether or not debuff effects are enabled
+    var debuffs: Boolean by property("debuffs", true)
+
+    // The chance at which debuff effects can occur (default is 5%)
+    var debuffChance: Double by property("debuff-chance", 0.05)
 
     // Netherite grants the max amount of boost per block
     val netheriteBoost: Int
