ca98358f818111dbb22a5375adae00501d3b7e55
Atrius <atriusedm@gmail.com>
2020-09-22 01:28:18 -0400

    QoL method for cancelling events

--- src/main/kotlin/xyz/atrius/waystones/utility/EventUtils.kt
+++ src/main/kotlin/xyz/atrius/waystones/utility/EventUtils.kt
@@ -1,6 +1,11 @@
 package xyz.atrius.waystones.utility
 
+import org.bukkit.event.Cancellable
 import org.bukkit.event.player.PlayerMoveEvent
 
 fun PlayerMoveEvent.hasMovedBlock() =
     from.toVector().toBlockVector() != to?.toVector()?.toBlockVector()
+
+fun Cancellable.cancel() {
+    isCancelled = true
+}
