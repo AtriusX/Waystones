1386e2f45be7ba122ec888c0fa6faff9d2a6f144
Atrius <atriusedm@gmail.com>
2020-09-22 01:27:30 -0400

    Added method to check if player is immortal

--- src/main/kotlin/xyz/atrius/waystones/utility/PlayerUtils.kt
+++ src/main/kotlin/xyz/atrius/waystones/utility/PlayerUtils.kt
@@ -3,8 +3,12 @@ package xyz.atrius.waystones.utility
 import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.ChatMessageType
 import net.md_5.bungee.api.chat.TextComponent
+import org.bukkit.GameMode
 import org.bukkit.Sound
 import org.bukkit.entity.Player
+
+val Player.immortal: Boolean
+    get() = gameMode in listOf(GameMode.CREATIVE, GameMode.SPECTATOR)
 
 fun Player.sendActionMessage(message: String, color: ChatColor? = null) =
     this.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent("${color ?: ""}$message"))
