5a7633a4ca074bb2076a559e3e6f4868dfb52e95
Atrius <atriusedm@gmail.com>
2020-09-23 15:13:20 -0400

    Reorganize config options

--- src/main/kotlin/xyz/atrius/waystones/data/Config.kt
+++ src/main/kotlin/xyz/atrius/waystones/data/Config.kt
@@ -20,15 +20,6 @@ class Config(
     // Whether or not the player receiving damage will force cancel the warp
     var damageStopsWarping: Boolean by property("damage-stops-warping", true)
 
-    // The max number of blocks allowed in warp structures
-    var maxWarpSize: Int by property("max-warp-size", 25)
-
-    // Allow users to jump between dimensions when warping
-    var jumpWorlds: Boolean by property("jump-worlds", true)
-
-    // Play animations during teleport sequence
-    var warpAnimations: Boolean by property("warp-animations", true)
-
     // Determines if teleporters limit their max distance or not, will check if user is in range before teleporting if true
     var limitDistance: Boolean by property("limit-distance", true)
 
@@ -38,15 +29,23 @@ class Config(
     // The maximum amount of blocks a portal block can boost the range of a portal (this is for netherite)
     var maxBoost: Int by property("max-boost", 50)
 
+    // The max number of blocks allowed in warp structures
+    var maxWarpSize: Int by property("max-warp-size", 25)
+
+    // Allow users to jump between dimensions when warping
+    var jumpWorlds: Boolean by property("jump-worlds", true)
+
+    // How many blocks it takes to travel one block in the destination world
+    var worldRatio: Int by property("world-ratio", 8)
+
+    // Play animations during teleport sequence
+    var warpAnimations: Boolean by property("warp-animations", true)
+
     // Determines whether or not warping with a compass deletes the item after use
     var singleUse: Boolean by property("single-use", false)
 
     // Determines if/how power is required from a respawn anchor in order to use the warp
     var requirePower: Power by enumProp("require-power", Power.INTER_DIMENSION)
-
-    // How many blocks it takes to travel one block in the destination world
-    var worldRatio: Int by property("world-ratio", 8)
-
     // Whether or not debuff effects are enabled
     var portalSickness: Boolean by property("enable-portal-sickness", true)
 
