397b0af309b2dcdd53429047c3aa1c07bb8c69c2
Atrius <atriusedm@gmail.com>
2020-09-23 05:10:52 -0400

    Add warp key utilities

--- src/main/kotlin/xyz/atrius/waystones/utility/PluginUtils.kt
+++ src/main/kotlin/xyz/atrius/waystones/utility/PluginUtils.kt
@@ -1,13 +1,35 @@
 package xyz.atrius.waystones.utility
 
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.event.Listener
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.CompassMeta
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.PluginManager
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitScheduler
+import xyz.atrius.waystones.data.Config
 
 // Just because I'm petty
 typealias KotlinPlugin =
         JavaPlugin
+
+fun keyValue(plugin: KotlinPlugin) =
+    NamespacedKey(plugin, "is_warp_key")
+
+fun defaultWarpKey(plugin: KotlinPlugin): ItemStack = ItemStack(Material.COMPASS).update<CompassMeta> {
+    persistentDataContainer[keyValue(plugin), PersistentDataType.INTEGER] = 1
+    lore = listOf(
+        "${ChatColor.DARK_PURPLE}Warpstone: [${ChatColor.MAGIC}UNKNOWN${ChatColor.DARK_PURPLE}]"
+    )
+    setDisplayName("${ChatColor.GOLD}Warpstone Key")
+}
+
+fun ItemStack.isWarpKey(plugin: KotlinPlugin, config: Config) = if (config.keyItems)
+    itemMeta?.persistentDataContainer?.get(keyValue(plugin), PersistentDataType.INTEGER) == 1
+else type == Material.COMPASS
 
 fun PluginManager.registerEvents(plugin: JavaPlugin, vararg listeners: Listener) =
     listeners.forEach { registerEvents(it, plugin) }
