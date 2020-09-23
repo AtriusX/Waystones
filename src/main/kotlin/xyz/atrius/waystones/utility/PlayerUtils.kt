dc3509c878eb87dac97e57666fa05a5e2b019d88
Atrius <atriusedm@gmail.com>
2020-09-22 21:44:16 -0400

    Add new addOrStackItem utility for handling items

--- src/main/kotlin/xyz/atrius/waystones/utility/PlayerUtils.kt
+++ src/main/kotlin/xyz/atrius/waystones/utility/PlayerUtils.kt
@@ -6,6 +6,8 @@ import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.GameMode
 import org.bukkit.Sound
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.PlayerInventory
 import org.bukkit.potion.PotionEffectType
 
 val Player.immortal: Boolean
@@ -22,3 +24,21 @@ fun Player.playSound(sound: Sound, volume: Float = 1f, pitch: Float = 1f) =
 
 fun Player.hasPortalSickness() =
     getPotionEffect(PotionEffectType.CONFUSION)?.amplifier ?: 0 >= 4
+
+fun PlayerInventory.addOrStackItem(original: ItemStack, new: ItemStack) {
+    when {
+        holder is Player && (holder as Player).immortal ->
+            addItem(new)
+        original.amount == 1 ->
+            if (any { it?.isSimilar(new) == true && it.amount < it.maxStackSize }) {
+                addItem(new)
+                original.amount--
+            } else {
+                original.itemMeta = new.itemMeta
+            }
+        else -> {
+            addItem(new)
+            original.amount--
+        }
+    }
+}
