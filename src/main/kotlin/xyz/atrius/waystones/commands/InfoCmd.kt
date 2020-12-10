95689fcce391b14ed15c511675d2d2307aa71a77
Jess <sepshuncontact@gmail.com>
2020-12-09 19:51:06 -0500

    feat(InfoCmd): display version number

--- src/main/kotlin/xyz/atrius/waystones/commands/InfoCmd.kt
+++ src/main/kotlin/xyz/atrius/waystones/commands/InfoCmd.kt
@@ -1,11 +1,16 @@
 package xyz.atrius.waystones.commands
 
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
+import org.bukkit.plugin.Plugin
 
 fun infoCmd (
     sender: CommandSender
 ): Boolean {
+    val plugin = Bukkit.getPluginManager().getPlugin("Waystones")
+    Bukkit.getLogger()
     sender.sendMessage("------------ §dWaystones§r ------------")
+    sender.sendMessage("Version: §a${plugin?.description?.version}")
     sender.sendMessage("Author: §dAtriusX§r")
     sender.sendMessage("§aCommands: §r")
     sender.sendMessage("§a/waystones reload§r - Reloads plugin config")
