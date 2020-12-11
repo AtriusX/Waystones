1ed68b2fe11d2fdcba9a7e34846f4e841a3c62e1
Atrius <atriusedm@gmail.com>
2020-12-11 15:23:27 -0500

    Simplify InfoEvent

--- src/main/kotlin/xyz/atrius/waystones/event/InfoEvent.kt
+++ src/main/kotlin/xyz/atrius/waystones/event/InfoEvent.kt
@@ -8,32 +8,26 @@ import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import xyz.atrius.waystones.service.WarpNameService
 import xyz.atrius.waystones.utility.*
-import xyz.atrius.waystones.utility.WarpState.*
 
 class InfoEvent(private val names : WarpNameService) : Listener {
 
     @EventHandler
     fun onClick(event: PlayerInteractEvent) {
+        // Filter non left-click events
         if (event.action != Action.LEFT_CLICK_BLOCK)
             return
         val block  = event.clickedBlock
         val player = event.player
         val item   = player.inventory.itemInMainHand
-        if (block?.type == Material.LODESTONE && item.isWarpKey()) {
-            player.sendActionMessage(
-                "Name: ${
-                    names[block.location] ?: "None"
-                } ${
-                    when(block.getWarpState(player)) {
-                        Infinite -> "| Range: Infinite"
-                        Inhibited  -> "| Status: Inhibited"
-                        Unpowered  -> "| Status: Unpowered"
-                        Obstructed -> "| Status: Obstructed"
-                        else       -> "| Range: ${block.location.range()}"
-                    }
-                }", ChatColor.DARK_AQUA
-            )
-            event.cancel()
-        }
+        // Make sure the correct block/item pair is used
+        if (block?.type != Material.LODESTONE || !item.isWarpKey())
+            return
+        val name = names[block.location] ?: "None"
+        val state = block.getWarpState(player)
+        // Skip any non-warp blocks
+        if (state == WarpState.None)
+            return
+        player.sendActionMessage("Name: $name | $state", ChatColor.DARK_AQUA)
+        event.cancel()
     }
 }
