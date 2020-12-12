7264075456207710699bd27e81f39788bb046b59
Atrius <atriusedm@gmail.com>
2020-12-11 20:06:00 -0500

    Add operators for item meta to simplify the storage and retrieval process

--- src/main/kotlin/xyz/atrius/waystones/utility/ItemUtils.kt
+++ src/main/kotlin/xyz/atrius/waystones/utility/ItemUtils.kt
@@ -1,17 +1,26 @@
 package xyz.atrius.waystones.utility
 
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.CompassMeta
+import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.persistence.PersistentDataType.INTEGER
 import xyz.atrius.waystones.configuration
-import xyz.atrius.waystones.utility.KeyState.*
+import xyz.atrius.waystones.data.KeyState
+import xyz.atrius.waystones.data.KeyState.*
+import xyz.atrius.waystones.plugin
 
+operator fun ItemMeta.get(key: String) =
+        persistentDataContainer.get(NamespacedKey(plugin, key), INTEGER)
+
+operator fun <T> ItemMeta.set(key: String, type: PersistentDataType<T, T>, value: T) =
+        persistentDataContainer.set(NamespacedKey(plugin, key), type, value)
 
 fun ItemStack.isWarpKey() = if (configuration.keyItems)
-    itemMeta?.persistentDataContainer?.get(keyValue(), INTEGER) == 1
-else type == Material.COMPASS
+    itemMeta?.get("is_warp_key") == 1 else type == Material.COMPASS
 
 fun CompassMeta.isSevered(): Boolean =
     !hasLodestone() && isLodestoneTracked
