ae3e8a9757eb6cefb20b2c70395030489521ac45
Atrius <atriusedm@gmail.com>
2020-12-12 05:43:43 -0500

    Implement property objects into config

---
+++ src/main/kotlin/xyz/atrius/waystones/data/Property.kt
@@ -0,0 +1,31 @@
+package xyz.atrius.waystones.data
+
+import org.bukkit.configuration.file.FileConfiguration
+import xyz.atrius.waystones.data.config.ArgumentParser
+import xyz.atrius.waystones.plugin
+import kotlin.properties.Delegates.observable
+import kotlin.reflect.KProperty
+
+class Property<T>(
+    property: String,
+    default: T,
+    private val parser: ArgumentParser<T>
+) {
+    private val config: FileConfiguration
+        get() = plugin.config
+
+    private var value: T by observable(
+        parser.parse(config.getString(property)) ?: default, observe(property)
+    )
+
+    operator fun invoke() = value
+
+    operator fun invoke(input: String) {
+        value = parser.parse(input) ?: return
+    }
+
+    private fun <T> observe(property: String) = { _: KProperty<*>, _: T, new: T ->
+        plugin.config.set(property, new)
+        plugin.saveConfig()
+    }
+}
--- src/main/kotlin/xyz/atrius/waystones/data/config/Config.kt
+++ src/main/kotlin/xyz/atrius/waystones/data/config/Config.kt
@@ -2,12 +2,13 @@ package xyz.atrius.waystones.data.config
 
 import org.bukkit.configuration.file.FileConfiguration
 import xyz.atrius.waystones.Power
+import xyz.atrius.waystones.Power.INTER_DIMENSION
 import xyz.atrius.waystones.SicknessOption
+import xyz.atrius.waystones.SicknessOption.DAMAGE_ON_TELEPORT
+import xyz.atrius.waystones.data.Property
 import xyz.atrius.waystones.utility.KotlinPlugin
-import xyz.atrius.waystones.utility.valueOfOrDefault
-import kotlin.properties.Delegates
-import kotlin.reflect.KProperty
 
+@Suppress("MemberVisibilityCanBePrivate")
 class Config(
     private val plugin: KotlinPlugin
 ) {
@@ -15,92 +16,90 @@ class Config(
         get() = plugin.config
 
     // The amount of ticks to wait before teleporting
-    var waitTime: Int by property("wait-time", 60)
+    val waitTime: Property<Int> =
+        Property("wait-time", 60, PositiveValueParser)
 
     // Whether or not the player receiving damage will force cancel the warp
-    var damageStopsWarping: Boolean by property("damage-stops-warping", true)
+    val damageStopsWarping: Property<Boolean> =
+        Property("damage-stops-warping", true, BooleanParser)
 
     // Determines if teleporters limit their max distance or not, will check if user is in range before teleporting if true
-    var limitDistance: Boolean by property("limit-distance", true)
+    val limitDistance: Property<Boolean> =
+        Property("limit-distance", true, BooleanParser)
 
     // The minimum base range a waystone can have
-    var baseDistance: Int by property("base-distance", 100)
+    val baseDistance: Property<Int> =
+        Property("base-distance", 100, PositiveValueParser)
 
     // The maximum amount of blocks a portal block can boost the range of a portal (this is for netherite)
-    var maxBoost: Int by property("max-boost", 50)
+    val maxBoost: Property<Int> =
+        Property("max-boost", 50, PositiveValueParser)
 
     // The max number of blocks allowed in warp structures
-    var maxWarpSize: Int by property("max-warp-size", 25)
+    val maxWarpSize: Property<Int> =
+        Property("max-warp-size", 25, PositiveValueParser)
 
     // Allow users to jump between dimensions when warping
-    var jumpWorlds: Boolean by property("jump-worlds", true)
+    val jumpWorlds: Property<Boolean> =
+        Property("jump-worlds", true, BooleanParser)
 
     // How many blocks it takes to travel one block in the destination world
-    var worldRatio: Int by property("world-ratio", 8)
+    val worldRatio: Property<Int> =
+        Property("world-ratio", 8, PositiveValueParser)
 
     // Play animations during teleport sequence
-    var warpAnimations: Boolean by property("warp-animations", true)
+    val warpAnimations: Property<Boolean> =
+        Property("warp-animations", true, BooleanParser)
 
     // Determines whether or not warping with a compass deletes the item after use
-    var singleUse: Boolean by property("single-use", false)
+    val singleUse: Property<Boolean> =
+        Property("single-use", false, BooleanParser)
 
     // Determines if/how power is required from a respawn anchor in order to use the warp
-    var requirePower: Power by enumProp("require-power", Power.INTER_DIMENSION)
+    val requirePower: Property<Power> =
+        Property("require-power", INTER_DIMENSION, EnumParser(Power::class))
 
     // Whether or not debuff effects are enabled
-    var portalSickness: Boolean by property("enable-portal-sickness", true)
+    val portalSickness: Property<Boolean> =
+        Property("enable-portal-sickness", true, BooleanParser)
 
     // The chance at which debuff effects can occur (default is 5%)
-    var portalSicknessChance: Double by property("portal-sickness-chance", 0.05)
+    val portalSicknessChance: Property<Double> =
+        Property("portal-sickness-chance", 0.05, PercentageParser)
 
-    var portalSickWarping: SicknessOption by enumProp("portal-sickness-warping", SicknessOption.DAMAGE_ON_TELEPORT)
+    // Defines the behavior of warping while under the portal sickness effect
+    val portalSickWarping: Property<SicknessOption> =
+        Property("portal-sickness-warping", DAMAGE_ON_TELEPORT, EnumParser(SicknessOption::class))
 
-    var portalSicknessDamage: Double by property("portal-sickness-damage", 5.0)
+    // The amount of damage done by portal sickness
+    val portalSicknessDamage: Property<Double> =
+        Property("portal-sickness-damage", 5.0, DoubleParser)
 
     // Whether or not keys can be relinked to another warpstone after being linked once
-    var relinkableKeys: Boolean by property("relinkable-keys", true)
+    val relinkableKeys: Property<Boolean> =
+        Property("relinkable-keys", true, BooleanParser)
 
-    var keyItems: Boolean by property("enable-key-items", true)
+    // Whether or not the plugin uses a custom key item for warp keys
+    val keyItems: Property<Boolean> =
+        Property("enable-key-items", true, BooleanParser)
 
     // Netherite grants the max amount of boost per block
-    val netheriteBoost: Int
-        get() = maxBoost
+    fun netheriteBoost(): Int = maxBoost()
+
     // Emerald grants 75% of the max boost per block
-    val emeraldBoost: Int
-        get() = (maxBoost * 0.75).toInt()
+    fun emeraldBoost(): Int = (maxBoost() * 0.75).toInt()
+
     // Diamond grants 50% of the max boost per block
-    val diamondBoost: Int
-        get() = maxBoost / 2
+    fun diamondBoost(): Int = maxBoost() / 2
+
     // Gold grants 33% of the max boost per block
-    val goldBoost: Int
-        get() = maxBoost / 3
+    fun goldBoost(): Int = maxBoost() / 3
+
     // Iron grants 20% of the max boost per block
-    val ironBoost: Int
-        get() = maxBoost / 5
+    fun ironBoost(): Int = maxBoost() / 5
 
     init {
         plugin.config.options().copyDefaults(true)
         plugin.saveDefaultConfig()
-    }
-
-    fun <T> update(property: String, new: T) {
-        config.set(property, new)
-        plugin.saveConfig()
-    }
-
-    private inline fun <reified T> property(
-            property: String, default: T
-    ) = Delegates.observable(
-        config.get(property) as? T? ?: default, observe(property)
-    )
-
-    private inline fun <reified T : Enum<T>> enumProp(
-            property: String, default: T
-    ) = Delegates.observable(
-        valueOfOrDefault(config.getString(property), default), observe(property)
-    )
-
-    private fun <T> observe(property: String) = { _: KProperty<*>, _: T, new: T ->
-        update(property, new)
     }
 }
