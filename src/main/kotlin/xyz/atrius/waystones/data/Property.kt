e0b0e4a6c3875bf70e3410032082a708f918c5a2
Atrius <atriusedm@gmail.com>
2021-02-28 03:29:14 -0500

    Introduce IR Backend and format parsing line

--- src/main/kotlin/xyz/atrius/waystones/data/Property.kt
+++ src/main/kotlin/xyz/atrius/waystones/data/Property.kt
@@ -19,16 +19,17 @@ class Property<T>(
         ConfigManager.register(property, this)
     }
 
-    private var value: T by observable( // TODO: Test auto-define experiment
-        parser.parse(config.getString(property)) ?:
-            default.also { value = it }, observe(property)
-    )
-
     operator fun invoke(): T = value
 
     operator fun invoke(input: String) {
         value = parser.parse(input) ?: return
     }
+
+    private var value: T by observable( // TODO: Test auto-define experiment
+        parser.parse(config.getString(property))
+            ?: default.also { value = it },
+        observe(property)
+    )
 
     private fun <T> observe(property: String) = { _: KProperty<*>, _: T, new: T ->
         plugin.config.set(property, new)
