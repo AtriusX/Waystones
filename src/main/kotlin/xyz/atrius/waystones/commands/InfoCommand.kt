2dac51786c91b9dc4c723988a0c2b3c006173f4c
Jess <sepshuncontact@gmail.com>
2021-03-03 03:12:47 -0500

    feat(InfoCommand): add "i" alias

--- src/main/kotlin/xyz/atrius/waystones/commands/InfoCommand.kt
+++ src/main/kotlin/xyz/atrius/waystones/commands/InfoCommand.kt
@@ -4,7 +4,7 @@ import org.bukkit.command.CommandSender
 import xyz.atrius.waystones.plugin
 import xyz.atrius.waystones.utility.translateColors
 
-object InfoCommand : SimpleCommand("info") {
+object InfoCommand : SimpleCommand("info", "i") {
 
     override fun execute(sender: CommandSender, args: Array<String>) {
         sender.sendMessage("""
