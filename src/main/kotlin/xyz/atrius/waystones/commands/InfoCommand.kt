bcc6bb8bcd114a2a1700bcd81ee8cf3ba9d63b4e
Jess <sepshuncontact@gmail.com>
2021-03-05 03:25:43 -0500

    refactor(InfoCommand): improve format & show contributors
    Add line for contributors on the project
    Improve formatting, don't recalculate on every execution

--- src/main/kotlin/xyz/atrius/waystones/commands/InfoCommand.kt
+++ src/main/kotlin/xyz/atrius/waystones/commands/InfoCommand.kt
@@ -6,15 +6,22 @@ import xyz.atrius.waystones.utility.message
 
 object InfoCommand : SimpleCommand("info", "i") {
 
-    override fun execute(sender: CommandSender, args: Array<String>) {
-        sender.message("""
-            |
-            |&7------------- &dWaystones &7------------&r
-            |
-            |Version&7: &a${plugin.description.version}&r
-            |Author&7: &dAtri&r
-            |
-            |&7---------------&b-&d-&f-&d-&b-&7---------------
-        """.trimMargin())
-    }
+    val desc = plugin.description
+    val version = desc.version
+    val author = desc.authors.getOrNull(0) ?: "Unknown"
+    val contributors = desc.authors.drop(1)
+        .joinToString(", ") { "&d$it&r" }
+    val infoMsg = """
+        |
+        |&7------------- &dWaystones &7------------&r
+        |
+        |Version&7: &a$version&r
+        |Author&7: &d$author&r
+        |Contributors: $contributors
+        |
+        |&7---------------&b-&d-&f-&d-&b-&7---------------
+    """.trimIndent()
+
+    override fun execute(sender: CommandSender, args: Array<String>) =
+        sender.message(infoMsg)
 }
