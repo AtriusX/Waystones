7cbe4bf7f0cdc37c6a6d5a1314095addcbb3415b
Atrius <atriusedm@gmail.com>
2021-03-05 03:50:17 -0500

    Add website to info

--- src/main/kotlin/xyz/atrius/waystones/commands/InfoCommand.kt
+++ src/main/kotlin/xyz/atrius/waystones/commands/InfoCommand.kt
@@ -11,6 +11,7 @@ object InfoCommand : SimpleCommand("info", "i") {
     private val author = desc.authors.getOrNull(0) ?: "Unknown"
     private val contributors = desc.authors.drop(1)
         .joinToString(", ") { "&d$it&r" }
+    private val website = desc.website
     private val infoMsg = """
         |
         |&7------------- &dWaystones &7------------&r
@@ -18,6 +19,7 @@ object InfoCommand : SimpleCommand("info", "i") {
         |Version&7: &a$version&r
         |Author&7: &d$author&r
         |Contributors: $contributors
+        |Website: &d&n$website&r
         |
         |&7---------------&b-&d-&f-&d-&b-&7---------------
     """.trimIndent().translateColors()
