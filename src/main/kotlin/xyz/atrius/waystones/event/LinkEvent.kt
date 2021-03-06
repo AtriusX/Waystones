fd32d1046435d581735d3c502a5b9ae191466a31
Atrius <atriusedm@gmail.com>
2021-03-06 01:00:27 -0500

    Remove unused import

--- src/main/kotlin/xyz/atrius/waystones/event/LinkEvent.kt
+++ src/main/kotlin/xyz/atrius/waystones/event/LinkEvent.kt
@@ -4,7 +4,6 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
-import xyz.atrius.waystones.handler.HandleState
 import xyz.atrius.waystones.handler.HandleState.*
 import xyz.atrius.waystones.handler.LinkHandler
 import xyz.atrius.waystones.service.WarpNameService
