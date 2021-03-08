6181207d8cb6829a06daa0bb9cb63e08fd5cf92b
Atrius <atriusedm@gmail.com>
2021-03-08 00:34:16 -0500

    Move save logic to JsonFile.kt

--- src/main/kotlin/xyz/atrius/waystones/data/JsonFile.kt
+++ src/main/kotlin/xyz/atrius/waystones/data/JsonFile.kt
@@ -3,10 +3,16 @@ package xyz.atrius.waystones.data
 import com.google.gson.GsonBuilder
 import xyz.atrius.waystones.plugin
 import java.io.File
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption.CREATE
+import java.nio.file.StandardOpenOption.WRITE
 
-open class JsonFile(name: String) {
-    internal val file = File(plugin.dataFolder, "$name.json")
-    internal val json = GsonBuilder().setPrettyPrinting().create()
+open class JsonFile<T>(name: String) {
+    private val file = File(plugin.dataFolder, "$name.json")
+    private val json = GsonBuilder().setPrettyPrinting().create()
+    protected val data: HashMap<String, T> =
+        json.fromJson(FileReader(file), HashMap<String, T>()::class.java)
 
     init {
         load()
@@ -14,9 +20,15 @@ open class JsonFile(name: String) {
 
     private fun load() {
         try {
-            if (!file.exists()) plugin.saveResource(file.name, false)
+            if (!file.exists())
+                plugin.saveResource(file.name, false)
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
+
+    protected fun save() {
+        val json = json.toJson(data)
+        Files.write(file.toPath(), json.toByteArray(), CREATE, WRITE)
     }
 }
--- src/main/kotlin/xyz/atrius/waystones/service/WarpNameService.kt
+++ src/main/kotlin/xyz/atrius/waystones/service/WarpNameService.kt
@@ -3,30 +3,19 @@ package xyz.atrius.waystones.service
 import org.bukkit.Location
 import xyz.atrius.waystones.data.JsonFile
 import xyz.atrius.waystones.utility.locationCode
-import java.io.FileReader
-import java.nio.file.Files
-import java.nio.file.StandardOpenOption.CREATE
-import java.nio.file.StandardOpenOption.WRITE
 
-object WarpNameService : JsonFile("warpnames") {
-    private val names: HashMap<String, String> =
-        json.fromJson(FileReader(file), HashMap<String, String>()::class.java)
+object WarpNameService : JsonFile<String>("warpnames") {
 
     fun add(location: Location, name: String) {
-        names[location.locationCode] = name
-        saveFile()
+        data[location.locationCode] = name
+        save()
     }
 
     fun remove(location: Location?) {
-        names.remove(location?.locationCode)
-        saveFile()
-    }
-
-    private fun saveFile() {
-        val json = json.toJson(names)
-        Files.write(file.toPath(), json.toByteArray(), CREATE, WRITE)
+        data.remove(location?.locationCode)
+        save()
     }
 
     operator fun get(location: Location?) =
-        names[location?.locationCode]
+        data[location?.locationCode]
 }
