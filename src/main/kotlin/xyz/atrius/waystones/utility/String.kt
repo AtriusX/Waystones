87fe5f59c8a7162a2742f1af0b47c1e703d9ef8f
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-03-22 16:02:55 +0800

    Remove unused String.pluralize

--- src/main/kotlin/xyz/atrius/waystones/utility/String.kt
+++ src/main/kotlin/xyz/atrius/waystones/utility/String.kt
@@ -7,15 +7,6 @@ import xyz.atrius.waystones.plugin
 // Translate ChatColors using custom ColorCode
 fun String.translateColors(colorCode: Char = '&') = ChatColor.translateAlternateColorCodes(colorCode, this)
 
-// Return an 's' depending on input amount
-fun String.pluralize(vararg count: Int): String {
-    var result = this
-    count.forEach {
-        result = result.replaceFirst("(s)", if (it == 1) "" else "s")
-    }
-    return result
-}
-
 // Convert string to a namespace key
 fun String.toKey() = NamespacedKey(plugin, this)
 
