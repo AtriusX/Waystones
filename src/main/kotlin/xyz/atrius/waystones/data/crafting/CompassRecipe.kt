1d1604b33ed2827d907e0dbb9f343c6404e1994f
Atrius <atriusedm@gmail.com>
2021-06-13 03:35:20 -0400

    Generate recipe from material list in configuration

--- src/main/kotlin/xyz/atrius/waystones/data/crafting/CompassRecipe.kt
+++ src/main/kotlin/xyz/atrius/waystones/data/crafting/CompassRecipe.kt
@@ -1,19 +1,26 @@
 package xyz.atrius.waystones.data.crafting
 
 import org.bukkit.Material
+import xyz.atrius.waystones.configuration
 import xyz.atrius.waystones.utility.defaultWarpKey
 import xyz.atrius.waystones.utility.toKey
+import kotlin.math.sqrt
 
 object CompassRecipe : CraftingRecipe("is_warp_key".toKey(), defaultWarpKey()) {
 
-    override val recipe = """
-        | * 
-        |*x*
-        | * 
-    """.trimMargin()
+    override val recipe = run {
+        // Pull recipe from config and determine grid size (list can be size 1, 4, or 9)
+        val recipe = configuration.keyRecipe()
+        val size = sqrt(recipe.size.toFloat()).toInt()
+        // Map each item to a hashcode and join them into a multiline string
+        recipe.map { it.hashCode() }
+            .chunked(size).joinToString("\n") { it.joinToString("") }
+    }
 
-    override val items = hashMapOf(
-        '*' to Material.IRON_INGOT,
-        'x' to Material.REDSTONE_BLOCK
-    )
+    override val items = hashMapOf<Char, Material>().apply {
+        // For each item in the recipe, map it's material to it's hashcode
+        for (item in configuration.keyRecipe().toHashSet())
+            this[item.hashCode().toChar()] =
+                if (item.isEmpty()) Material.AIR else Material.valueOf(item)
+    }
 }
