cc83e5c345f3d224015de947717c301a609c1a45
Atrius <atriusedm@gmail.com>
2021-06-28 19:29:57 -0400

    Update command to handle /waystones getkey from console.

--- src/main/kotlin/xyz/atrius/waystones/commands/GetKeyCommand.kt
+++ src/main/kotlin/xyz/atrius/waystones/commands/GetKeyCommand.kt
@@ -9,7 +9,7 @@ import xyz.atrius.waystones.utility.message
 
 object GetKeyCommand : SimpleCommand("getkey", "gk", "key", "k") {
     // Calculate Player and Amount Values
-    private fun getPlayerAndAmount(default: Player, first: String?, second: String?): Pair<Player, Int> {
+    private fun getPlayerAndAmount(default: Player?, first: String?, second: String?): Pair<Player?, Int> {
         val player = if (first != null)
             Bukkit.getPlayer(first) ?: default else default
         val amount = second?.toIntOrNull() ?: first?.toIntOrNull() ?: 1
@@ -19,18 +19,17 @@ object GetKeyCommand : SimpleCommand("getkey", "gk", "key", "k") {
     override fun execute(sender: CommandSender, args: Array<String>) {
         // Set Amount and Player to give WarpKeys to
         val (player, amount) = getPlayerAndAmount(
-            sender as Player,
+            sender as? Player,
             args.getOrNull(0),
             args.getOrNull(1)
         )
+        // Return if selected player does not exist
+        player ?: return sender.message(localization["command-bad-sender", "console"])
         // Check Permissions
         if (!sender.hasPermission("waystones.getkey.self")
             || !sender.hasPermission("waystones.getkey.all")
             && sender != player
-        ) {
-            sender.message(localization["command-no-permission"])
-            return
-        }
+        ) return sender.message(localization["command-no-permission"])
         // Add WarpKey(s) to Player inventory
         player.inventory.addItem(defaultWarpKey(amount))
         // Inform Player of given WarpKey
