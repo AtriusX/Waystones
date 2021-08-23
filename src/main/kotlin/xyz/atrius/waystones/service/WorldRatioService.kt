e9d3a9cdde8dc471e691b4f4c85d4997d028ce78
Atrius <atriusedm@gmail.com>
2021-08-23 06:12:24 -0400

    Update ratio service to mirror the new values as well as add support for dynamically modifying the values

--- src/main/kotlin/xyz/atrius/waystones/service/WorldRatioService.kt
+++ src/main/kotlin/xyz/atrius/waystones/service/WorldRatioService.kt
@@ -6,5 +6,22 @@ import xyz.atrius.waystones.data.JsonFile
 object WorldRatioService : JsonFile<Double>("ratios") {
 
     operator fun get(world: World): Double =
-        data.getOrDefault(world.name, 1.0)
+        data.getOrDefault(world.environment.name.toLowerCase(), 1.0)
+
+    operator fun get(name: String, asEnvironment: Boolean): Double {
+        val key = if (asEnvironment) name else "name:$name"
+        return data.getOrDefault(key.toLowerCase(), 1.0)
+    }
+
+    fun add(value: String, asEnvironment: Boolean, ratio: Double) {
+        val key = if (asEnvironment) value else "name:$value"
+        data[key.toLowerCase()] = ratio
+        save(true)
+    }
+
+    fun remove(value: String, asEnvironment: Boolean) {
+        val key = if (asEnvironment) value else "name:$value"
+        data.remove(key)
+        save(true)
+    }
 }
