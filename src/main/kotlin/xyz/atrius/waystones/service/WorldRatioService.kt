b121a3dd501f17845aef0e08476444d1289adbef
Atrius <atriusedm@gmail.com>
2021-08-24 04:23:06 -0400

    Improve ratio service

--- src/main/kotlin/xyz/atrius/waystones/service/WorldRatioService.kt
+++ src/main/kotlin/xyz/atrius/waystones/service/WorldRatioService.kt
@@ -3,25 +3,28 @@ package xyz.atrius.waystones.service
 import org.bukkit.World
 import xyz.atrius.waystones.data.JsonFile
 
-object WorldRatioService : JsonFile<Double>("ratios") {
+object WorldRatioService : JsonFile<Double>("ratios"), Iterable<Map.Entry<String, Double>> {
 
-    operator fun get(world: World): Double =
-        data.getOrDefault(world.environment.name.toLowerCase(), 1.0)
-
-    operator fun get(name: String, asEnvironment: Boolean): Double {
-        val key = if (asEnvironment) name else "name:$name"
+    operator fun get(world: World, asEnvironment: Boolean = false): Double {
+        val key = if (asEnvironment) world.environment.name else "name:${world.name}"
         return data.getOrDefault(key.toLowerCase(), 1.0)
     }
 
-    fun add(value: String, asEnvironment: Boolean, ratio: Double) {
-        val key = if (asEnvironment) value else "name:$value"
+    fun add(world: World, asEnvironment: Boolean, ratio: Double) {
+        val key = if (asEnvironment) world.environment.name else "name:${world.name}"
         data[key.toLowerCase()] = ratio
         save(true)
     }
 
-    fun remove(value: String, asEnvironment: Boolean) {
-        val key = if (asEnvironment) value else "name:$value"
-        data.remove(key)
+    fun remove(world: World, asEnvironment: Boolean) {
+        val key = if (asEnvironment) world.environment.name else "name:${world.name}"
+        data.remove(key.toLowerCase())
         save(true)
     }
+
+    fun isEmpty(): Boolean =
+        data.isEmpty()
+
+    override fun iterator(): Iterator<Map.Entry<String, Double>> =
+        data.iterator()
 }
