dda811cc7f5dcd76329d5c7550325aca12caa979
Atrius <atriusedm@gmail.com>
2022-01-04 03:29:20 -0500

    Add warp service for all waystone data

---
+++ src/main/kotlin/xyz/atrius/waystones/service/WarpService.kt
@@ -0,0 +1,45 @@
+package xyz.atrius.waystones.service
+
+import org.bukkit.Location
+import xyz.atrius.waystones.data.JsonFile
+import xyz.atrius.waystones.data.config.LocationParser
+import xyz.atrius.waystones.data.json.Json
+import xyz.atrius.waystones.utility.locationCode
+
+data class Waystone(
+    val name: String?
+) : Json<Waystone>
+
+object WarpService : JsonFile<Waystone>("warps") {
+
+    /* This is temporary code that is used to migrate our existing data from
+     * our legacy name service. Once migration is complete the service will be
+     * deleted and will not be loaded on the next server restart.
+     *
+     * This migration feature should be removed in a later release.
+     */
+    override fun onLoad() {
+        // Check if an existing name file exists
+        if (WarpNameService.exists()) {
+            WarpNameService.load()
+            for ((code, name) in WarpNameService) {
+                val location = LocationParser.parse(code)
+                if (location != null)
+                    add(location, Waystone(name))
+            }
+            WarpNameService.delete()
+        }
+    }
+
+    fun add(location: Location, waystone: Waystone) = save {
+        data[location.locationCode] = waystone
+    }
+
+    fun remove(location: Location) = save {
+        data.remove(location.locationCode)
+    }
+
+    operator fun get(location: Location?): Waystone? =
+        data[location?.locationCode]
+}
+
