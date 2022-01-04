abf87113551c4da8fbb91d718f0b4874d980b105
Atrius <atriusedm@gmail.com>
2022-01-04 03:45:01 -0500

    Make plugin logger available outside plugin class scope and add logger messages to migrator

--- src/main/kotlin/xyz/atrius/waystones/Waystones.kt
+++ src/main/kotlin/xyz/atrius/waystones/Waystones.kt
@@ -13,10 +13,12 @@ import xyz.atrius.waystones.utility.KotlinPlugin
 import xyz.atrius.waystones.utility.registerEvents
 import xyz.atrius.waystones.utility.registerNamespaces
 import xyz.atrius.waystones.utility.registerRecipes
+import java.util.logging.Logger
 
 lateinit var plugin       : KotlinPlugin
 lateinit var configuration: Config
 lateinit var localization : Localization
+lateinit var log          : Logger
 
 @Suppress("unused")
 class Waystones : KotlinPlugin() {
@@ -25,6 +27,7 @@ class Waystones : KotlinPlugin() {
         plugin        = this
         configuration = Config(this)
         localization  = Localization(this)
+        log           = this.logger
         // Load services
         WarpService.load()
         WorldRatioService.load()
@@ -41,14 +44,14 @@ class Waystones : KotlinPlugin() {
         )
         // Register warp key recipe if enabled
         if (configuration.keyItems()) {
-            logger.info("Loading recipes!")
+            log.info("Loading recipes!")
             registerRecipes(
                 CompassRecipe
             )
         }
         // Register plugin advancements
         if (configuration.advancements()) {
-            logger.info("Loading advancements!")
+            log.info("Loading advancements!")
             AdvancementManager.register(
                 WAYSTONES,
                 SECRET_TUNNEL,
@@ -72,10 +75,10 @@ class Waystones : KotlinPlugin() {
                 RatioCommand
             )
         )
-        logger.info("Waystones loaded!")
+        log.info("Waystones loaded!")
     }
 
     override fun onDisable() {
-        logger.info("Waystones disabled!")
+        log.info("Waystones disabled!")
     }
 }
--- src/main/kotlin/xyz/atrius/waystones/service/WarpService.kt
+++ src/main/kotlin/xyz/atrius/waystones/service/WarpService.kt
@@ -4,6 +4,7 @@ import org.bukkit.Location
 import xyz.atrius.waystones.data.JsonFile
 import xyz.atrius.waystones.data.config.LocationParser
 import xyz.atrius.waystones.data.json.Json
+import xyz.atrius.waystones.log
 import xyz.atrius.waystones.utility.locationCode
 
 data class Waystone(
@@ -18,9 +19,11 @@ object WarpService : JsonFile<Waystone>("warps") {
      *
      * This migration feature should be removed in a later release.
      */
+    @Suppress("DEPRECATION")
     override fun onLoad() {
         // Check if an existing name file exists
         if (WarpNameService.exists()) {
+            log.info("Existing warpnames.json file detected! Attempting to migrate.")
             WarpNameService.load()
             for ((code, name) in WarpNameService) {
                 val location = LocationParser.parse(code)
@@ -28,6 +31,7 @@ object WarpService : JsonFile<Waystone>("warps") {
                     add(location, Waystone(name))
             }
             WarpNameService.delete()
+            log.info("Migration of warpnames.json complete.")
         }
     }
 
