8af67962dbfd36f472b32319a7ec6a0a4c8c9187
Atri <atriusedm@gmail.com>
2025-08-07 22:11:38 -0400

    chore: Add temporary migration command for waystone names
    This command is being introduced to allow for the end user to determine when they want to time their database migration. As the plugin provides an SQLite configuration out of the box, we should give server managers time to adjust the config should they decide they want to opt for a MySQL server connection instead.

--- src/main/kotlin/xyz/atrius/waystones/service/WarpNameService.kt
+++ src/main/kotlin/xyz/atrius/waystones/service/WarpNameService.kt
@@ -1,13 +1,57 @@
 package xyz.atrius.waystones.service
 
 import com.google.gson.Gson
+import org.bukkit.Bukkit
 import org.koin.core.annotation.Single
+import org.slf4j.LoggerFactory
+import xyz.atrius.waystones.dao.WaystoneInfo
 import xyz.atrius.waystones.data.JsonFile
 import xyz.atrius.waystones.internal.KotlinPlugin
+import xyz.atrius.waystones.repository.WaystoneInfoRepository
 
 @Single
 @Deprecated("Database support is included now, so this will be phased out in the future")
 class WarpNameService(
     gson: Gson,
     plugin: KotlinPlugin,
-) : JsonFile<HashMap<String, String>>("warpnames", gson, plugin, HashMap<String, String>()::class)
+    private val waystoneInfoRepository: WaystoneInfoRepository
+) : JsonFile<HashMap<String, String>>("warpnames", gson, plugin, HashMap<String, String>()::class) {
+
+    fun migrate() {
+        logger.info("Migrating waystone names to database storage...")
+
+        // Migrate old data to database format
+        for ((locationCode, waystoneName) in data) {
+            val (worldName, location) = locationCode
+                .split("@")
+            val world = Bukkit
+                .getWorld(worldName)
+                ?: continue
+            val (x, y, z) = location
+                .split(":")
+                .map { it.toIntOrNull() }
+            val info = WaystoneInfo(
+                worldUid = world.uid,
+                x = x ?: continue,
+                y = y ?: continue,
+                z = z ?: continue,
+                name = waystoneName,
+            )
+            // Save old data to new format
+            waystoneInfoRepository.save(info)
+        }
+
+        logger.info("Waystone info migration complete!")
+    }
+
+    fun clearData() {
+        // Delete old data
+        data.clear()
+        save()
+    }
+
+    companion object {
+        private val logger = LoggerFactory
+            .getLogger(WarpNameService::class.java)
+    }
+}
