2358b73770c28f17c3c8811761e0437a3a6041e7
Atrius <atriusedm@gmail.com>
2026-05-17 04:02:59 -0400

    build: Fix configuration cache issue in resource processing

--- buildSrc/src/main/kotlin/waystones.resource-processing.gradle.kts
+++ buildSrc/src/main/kotlin/waystones.resource-processing.gradle.kts
@@ -2,14 +2,26 @@ plugins {
     id("waystones.base")
 }
 
+val versionProp = providers.gradleProperty("version")
+val buildPaperVersionProp = providers.gradleProperty("buildPaperVersion")
+val pluginApiVersionProp = providers.gradleProperty("pluginApiVersion")
+val paperVersionsProp = providers.gradleProperty("paperVersions")
+val pluginWebsiteProp = providers.gradleProperty("pluginWebsite")
+
 tasks.processResources {
+    val version = versionProp.get()
+    val buildPaperVersion = buildPaperVersionProp.get()
+    val pluginApiVersion = pluginApiVersionProp.get()
+    val paperVersions = paperVersionsProp.get()
+    val pluginWebsite = pluginWebsiteProp.get()
+
     filesMatching("paper-plugin.yml") {
         expand(
-            "version" to providers.gradleProperty("version").get(),
-            "buildPaperVersion" to providers.gradleProperty("buildPaperVersion").get(),
-            "pluginApiVersion" to providers.gradleProperty("pluginApiVersion").get(),
-            "paperVersions" to providers.gradleProperty("paperVersions").get(),
-            "pluginWebsite" to providers.gradleProperty("pluginWebsite").get(),
+            "version" to version,
+            "buildPaperVersion" to buildPaperVersion,
+            "pluginApiVersion" to pluginApiVersion,
+            "paperVersions" to paperVersions,
+            "pluginWebsite" to pluginWebsite,
         )
     }
 }
